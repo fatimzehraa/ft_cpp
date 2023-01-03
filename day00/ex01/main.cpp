76788482105b7b8d226101325d563b589dd3e0c5
Fatima zahra El bouaazzaoui <fael-bou@e2r7p5.1337.ma>
2023-01-03 21:53:28 +0100

    getline back and atoi for index

--- day00/ex01/main.cpp
+++ day00/ex01/main.cpp
@@ -6,31 +6,45 @@
 #include "phonebook.hpp"
 #include <fstream>
 
+int	ft_atoi(std::string &str)
+{
+	int nb = 0;
+
+	if (str[0] < '0' || str[0] > '7' || str.size() != 1 ) {
+		return -1;
+	}
+		nb = str[0] - '0';
+	return (nb);
+}
+
 int main ()
 {
 	phonebook myphonebook;
 	std::string str;
-	int index;
-	int i = 1;
+	std::string index;
 
-	while(i) {
-		printf("i = %d\n", i);
-		std::cout << "insert a command, the program accepts only 3 commands which are ADD, SEARCH, and EXIT" << std::endl;
-		std::cin >> str;
-		std::cin.sync();
+	std::cout << "insert your command in the prompt, this program accepts only 3 commands which are ADD, SEARCH, and EXIT" << std::endl;
+	while(!std::cin.eof()) {
+		std::cout << "$> " << std::flush;
+		getline(std::cin, str);
+		//std::cin.clear();
+		//std::cin.ignore();
 		if (str.compare("ADD") == 0) {
 			myphonebook.add();
 		}
 		else if (str.compare("SEARCH") == 0){
 			myphonebook.show_contacts();
-			std::cin >> index;
-			std::cin.sync();
-			myphonebook.search(index);
+			getline(std::cin, index);
+			if (!std::cin.eof())
+				myphonebook.search(ft_atoi(index));
+			else
+				continue;
 		}
 		else if(str.compare("EXIT") == 0 || std::cin.eof()){
 			break;
 		}
-		i++;
+		else
+			std::cout << "command not found" << std::endl;
 	}
 //	std::cout << str << std::endl;
 	return 0;
--- day00/ex01/phonebook.cpp
+++ day00/ex01/phonebook.cpp
@@ -10,41 +10,34 @@ phonebook::phonebook(){
 //	contacts = new contact[8];
 }
 
+std::string get_info(std::string s)
+{
+	std::string val;
+	do
+	{
+		//if (!std::cin.eof())
+			std::cout << s << ": " << std::flush;
+		//std::cin.ignore();
+		getline(std::cin, val);
+	} while(!std::cin.eof() && val.empty());
+	std::cout << "val = " << val << std::endl;;
+	return val;
+}
+
 //static variable that re initialize when hits 9
 void phonebook::add(){
-	std::string first_name;
-	std::string last_name;
-	std::string nickname;
-	std::string phone_number;
-	std::string darkest_secret;
+	static int i;
 
-	if (size == 8)
-		size = 0;
-	std::cout << "enter first name" << std::endl;
-	std::cin >> first_name;
-	std::cin.sync();
-	contacts[size].set_first_name(first_name);
-
-	std::cout << "enter last name" << std::endl;
-	std::cin >> last_name;
-	std::cin.sync();
-	contacts[size].set_last_name(last_name);
-
-	std::cout << "enter nickname" << std::endl;
-	std::cin >> nickname;
-	std::cin.sync();
-	contacts[size].set_nickname(nickname);
-
-	std::cout << "enter phone number" << std::endl;
-	std::cin >> phone_number;
-	std::cin.sync();
-	contacts[size].set_phone_number(phone_number);
-
-	std::cout << "enter darkest secret" << std::endl;
-	std::cin >> darkest_secret;
-	std::cin.sync();
-	contacts[size].set_darkest_secret(darkest_secret);
-	size++;
+	if (i == 8)
+		i = 0;
+	contacts[i].set_first_name(get_info("enter first name"));
+	contacts[i].set_last_name(get_info("enter last name"));
+	contacts[i].set_nickname(get_info("enter nickname"));
+	contacts[i].set_phone_number(get_info("enter phone_number"));
+	contacts[i].set_darkest_secret(get_info("enter darkest_secret"));
+	if (size < 8)
+		size++;
+	i++;
 }
 void trim(std::string s){
 	if (s.size() < 10) {
@@ -73,14 +66,11 @@ void phonebook::show_contacts(){
 void phonebook::search(int index){
 	for (int i = 0; i < size; i++) {
 		if (index == i){
-			std::cout << "|     index|first name| last name|  nickname|phone num.|darkest s.|" << std::endl;
-			std::cout << "|" << std::setw(10) << i ;
-			trim(contacts[i].get_first_name());
-			trim(contacts[i].get_last_name());
-			trim(contacts[i].get_nickname());
-			trim(contacts[i].get_phone_number());
-			trim(contacts[i].get_darkest_secret());
-			std::cout << "|" << std::endl;
+			std::cout << "first name: " << contacts[i].get_first_name() << std::endl;
+			std::cout << "last name: " << contacts[i].get_last_name() << std::endl;
+			std::cout << "nickname: " << contacts[i].get_nickname() << std::endl;
+			std::cout << "phone_number: " << contacts[i].get_phone_number() << std::endl;
+			std::cout << "darkest_secret: " << contacts[i].get_darkest_secret() << std::endl;
 		}
 	}
 //	return NULL;
