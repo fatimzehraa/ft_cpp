1d9aac687eb44b806c9904b82ee87bd59d16399f
fatimzehraa <elbouaazzaouitema@gmail.com>
2023-02-14 22:53:01 +0100

    please study at home

---
+++ day08/ex00/easyfind.hpp
@@ -0,0 +1,76 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template <typename T>
+int easyfind(T container, int a){
+	
+}
+/*
+int easyfind(T container, int a)
+{
+	it = container.begin();
+	while(it != container.end())
+	{
+		print *it;
+		it++;
+	}
+}
+
+
+#include <vector>
+#include <list>
+
+class vec<T> {
+	class iterator{
+		int index;
+		T* arr;
+	
+		operator++(){
+			index++;
+		}
+	
+		operator*(){
+			return arr[index];
+		}
+	
+	}
+
+
+	iterator end(){
+		return iter_vec(arr, len)
+	}
+
+	iterator begin(){
+		iter_vec(arr, 0)
+	}
+
+
+}
+
+
+class iter_list: iterator {
+	list *node;
+
+	operator++{
+		node = node->next;
+	}
+
+	operator* {
+		return node->content;
+	}
+}
+
+
+
+int iter(){
+
+	std::list<int> vec(0, 5);
+	std::list<int>::iterator it = vec.begin();
+	std::list<int>::iterator end = vec.end();
+	while(it != end)
+	{
+	
+		it++;
+	}
+}*/
+#endif
